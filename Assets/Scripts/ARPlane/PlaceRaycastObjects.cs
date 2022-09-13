10f1d7194ee6b429092c745ee2e3c21d340b562b
Raphaguizan <raphaelguizan@gmail.com>
2022-09-13 19:47:32 -0300

    cena do plane tracking
    - adicionada cena para fazer o plane tracking
    - criado script para posicionar objetos
    - criado prefab default para posicionar no local clicado
    - botão para desligar o plano mapeado

---
+++ Assets/Scripts/ARPlane/PlaceRaycastObjects.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.XR.ARFoundation;
+
+[RequireComponent(typeof(ARRaycastManager))]
+public class PlaceRaycastObjects : MonoBehaviour
+{
+    private ARRaycastManager m_RaycastManager;
+    private List<ARRaycastHit> m_Hits = new List<ARRaycastHit>();
+    private Transform prefabTransform = null;
+
+
+    [SerializeField]
+    private GameObject m_Prefab;
+
+    private void Awake()
+    {
+        m_RaycastManager = GetComponent<ARRaycastManager>();
+    }
+
+    private void Update()
+    {
+        if (Input.touchCount == 0)
+            return;
+        if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
+        {
+            if (!EventSystem.current.IsPointerOverGameObject(Input.GetTouch(0).fingerId))
+            {
+                Debug.Log("Tocou na UI");
+                return;
+            }
+        }
+
+        Debug.Log("Tocou fora da UI");
+        if (m_RaycastManager.Raycast(Input.GetTouch(0).position, m_Hits))
+        {
+            PlacePrefab(m_Hits[0]);
+        }
+    }
+
+    private void PlacePrefab(ARRaycastHit hit)
+    {
+        if(prefabTransform == null)
+        {
+            var prefabAux = Instantiate(m_Prefab, hit.pose.position, hit.pose.rotation);
+            prefabTransform = prefabAux.transform;
+        }
+        else
+        {
+            prefabTransform.SetPositionAndRotation(hit.pose.position, hit.pose.rotation);
+        }
+    }
+}
---
+++ Assets/Scripts/ARPlane/TogglePlaneVisualizer.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine.XR.ARFoundation;
+
+public class TogglePlaneVisualizer : MonoBehaviour
+{
+    [SerializeField]
+    private Button button;
+    [SerializeField]
+    private string buttonOnText = "Desligar Plano";
+    [SerializeField]
+    private string buttonOffText = "Visualizar Plano";
+
+    private TextMeshProUGUI buttonText;
+
+    private ARPlaneManager planeManager;
+    private bool visualizerEnabled;
+    private void Awake()
+    {
+        planeManager = GetComponent<ARPlaneManager>();
+        button.onClick.AddListener(ToggleVisualizer);
+        buttonText = button.GetComponentInChildren<TextMeshProUGUI>();
+        SetPlaneVisualizer(false);
+    }
+
+    private void ToggleVisualizer()
+    {
+        SetPlaneVisualizer(!visualizerEnabled);
+    }
+
+    private void SetPlaneVisualizer(bool val)
+    {
+        foreach(var plane in planeManager.trackables)
+            plane.gameObject.SetActive(val);
+        
+        ChangeButtonText(val);
+        visualizerEnabled = val;
+    }
+
+    private void ChangeButtonText(bool val)
+    {
+        if (val)
+            buttonText.text = buttonOnText;
+        else
+            buttonText.text = buttonOffText;
+    }
+    private void OnDestroy()
+    {
+        button.onClick.RemoveAllListeners();
+    }
+}
