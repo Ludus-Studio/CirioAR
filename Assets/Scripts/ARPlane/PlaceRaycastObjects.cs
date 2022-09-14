ba8f1dcd7470a06883885029618743b4961e244a
Raphaguizan <raphaelguizan@gmail.com>
2022-09-14 17:00:37 -0300

    adição de rotação e escala do objeto

--- Assets/Scripts/ARPlane/PlaceRaycastObjects.cs
+++ Assets/Scripts/ARPlane/PlaceRaycastObjects.cs
@@ -4,6 +4,13 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.XR.ARFoundation;
 
+public enum TouchCountType
+{
+    None = 0,
+    Single = 1,
+    Double = 2
+}
+
 [RequireComponent(typeof(ARRaycastManager))]
 public class PlaceRaycastObjects : MonoBehaviour
 {
@@ -14,6 +21,10 @@ public class PlaceRaycastObjects : MonoBehaviour
 
     [SerializeField]
     private GameObject m_Prefab;
+    [SerializeField]
+    private float zoomThreshHolder = 0.005f;
+    [SerializeField]
+    private float rotateThreshHolder = 1f;
 
     private void Awake()
     {
@@ -22,19 +33,40 @@ public class PlaceRaycastObjects : MonoBehaviour
 
     private void Update()
     {
-        if (Input.touchCount == 0)
-            return;
-        if (Input.touchCount > 0 )
+        switch(CalculateTouch())
         {
-            if (EventSystem.current.IsPointerOverGameObject(Input.GetTouch(0).fingerId) 
+            case TouchCountType.None:
+                break;
+            case TouchCountType.Single:
+
+                if (m_RaycastManager.Raycast(Input.GetTouch(0).position, m_Hits))
+                    PlacePrefab(m_Hits[0]);
+
+                break;
+            case TouchCountType.Double:
+
+                RotateAndScalePrefab();
+
+                break;
+        }
+    }
+
+    private TouchCountType CalculateTouch()
+    {
+        if (Input.touchCount > 0)
+        {
+            if (EventSystem.current.IsPointerOverGameObject(Input.GetTouch(0).fingerId)
             || Input.GetTouch(0).phase == TouchPhase.Ended)
-                return;
+                return TouchCountType.None;
         }
 
-        if (m_RaycastManager.Raycast(Input.GetTouch(0).position, m_Hits))
-        {
-            PlacePrefab(m_Hits[0]);
-        }
+        if (Input.touchCount == 1)
+            return TouchCountType.Single;
+
+        if (Input.touchCount == 2)
+            return TouchCountType.Double;
+
+        return TouchCountType.None;
     }
 
     private void PlacePrefab(ARRaycastHit hit)
@@ -48,5 +80,33 @@ public class PlaceRaycastObjects : MonoBehaviour
         {
             prefabTransform.SetPositionAndRotation(hit.pose.position, hit.pose.rotation);
         }
+    }
+
+    private void RotateAndScalePrefab()
+    {
+        if (prefabTransform == null) return;
+
+        Touch touchZero = Input.GetTouch(0);
+        Touch touchOne = Input.GetTouch(1);
+
+        Vector2 touchZeroPrevPos = touchZero.position - touchZero.deltaPosition;
+        Vector2 touchOnePrevPos = touchOne.position - touchOne.deltaPosition;
+
+        // ZOOM
+        float prevMagnitude = (touchZeroPrevPos - touchOnePrevPos).magnitude;
+        float currentMagnitude = (touchZero.position - touchOne.position).magnitude;
+
+        float difference = currentMagnitude - prevMagnitude;
+
+        prefabTransform.localScale += Vector3.one * difference * zoomThreshHolder;
+        if(prefabTransform.localScale.x <= 0) prefabTransform.localScale = Vector3.zero + Vector3.one * zoomThreshHolder;
+
+        //ROTATE
+        Vector2 prevVector = (touchOnePrevPos - touchZeroPrevPos).normalized;
+        Vector2 currentVector = (touchOne.position - touchZero.position).normalized;
+
+        float angleDifference = Vector2.SignedAngle(prevVector, currentVector);
+
+        prefabTransform.Rotate(new Vector3(0, -angleDifference * rotateThreshHolder, 0), Space.World);
     }
 }
